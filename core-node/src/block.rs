0ed28ef561fb80d4441f51ed8c10506024b3449d
Lucien <lucien@wattcoin.com>
2026-09-22 19:00:35 +0200

    🔧 Fix 56 : repare pairs

--- core-node/src/block.rs
+++ core-node/src/block.rs
@@ -95,7 +95,7 @@ impl Block {
 
         let header = BlockHeader {
             index: 0,
-            timestamp: 1789979400, 
+            timestamp: 1790097000, 
             previous_hash: String::from("0000000000000000000000000000000000000000000000000000000000000000"),
             hash: String::from("GENESIS_HASH_WATTCOIN_000000000000000000000000000000000000000000"),
             nonce: 0,
